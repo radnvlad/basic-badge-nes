b284376bdc78f14844e87683564b45db77a5f9a3
Vlad Radulescu <vladradulescubv@yahoo.com>
2018-05-28 03:24:39 +0300

    Modified nes cartridge memory section.

--- firmware/badge1.X/prepare.sh
+++ firmware/badge1.X/prepare.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pushd ../../games
-xc32-objcopy --rename-section .data=.progmem.data,contents,alloc,load,readonly,data --input-target binary --output-target elf32-tradlittlemips --binary-architecture pic32mx SuperMarioBros.nes nesgame.o
+xc32-objcopy --rename-section .data=.progmem.text,contents,alloc,load,readonly,code --input-target binary --output-target elf32-tradlittlemips --binary-architecture pic32mx SuperMarioBros.nes nesgame.o
 popd 
